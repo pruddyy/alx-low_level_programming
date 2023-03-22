0e2a4b7da3780eded2455eb77367e1ecee683a49
pruddyy <kiokoprudence8@gmail.com>
2023-03-22 12:57:18 -0700

    E Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
  */
-int main(void)
+int add(int a, int b)
 {
-	times_table();
-	return (0);
+	int c;
+
+
+	c = a + b;
+
+
+	return (c);
 }
