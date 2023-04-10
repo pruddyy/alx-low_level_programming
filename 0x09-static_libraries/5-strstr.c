fee65a0a2500797baa2273905ee421829557ae8c
pruddyy <kiokoprudence8@gmail.com>
2023-04-10 01:48:07 -0700

    Lib E task

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -8,7 +8,7 @@
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
-		{
+	{
 		char *l = haystack;
 		char *p = needle;
 
@@ -20,5 +20,7 @@ char *_strstr(char *haystack, char *needle)
 
 		if (*p == '\0')
 			return (haystack);
-		}
+	}
+
+	return (0);
 }
